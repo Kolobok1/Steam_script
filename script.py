c601ac2de0905e2b226f86430979511ef5833871
Kolobok1 <73997845+Kolobok1@users.noreply.github.com>
2023-07-16 14:56:30 +0300

    Update script.py

--- script.py
+++ script.py
@@ -2,85 +2,81 @@ import json,pickle
 from steampy.models import GameOptions, Currency
 from bs4 import BeautifulSoup
 import requests, re
-import time, asyncio
+import time
 from headers import headers
 
+
 def proxies_list(n):
+
     login = ''
     password = ''
 
     proxies_list = [
         {
-            'http': f'socks5://{login}:{password}@',
-            'https': f'socks5://{login}:{password}@'            
+            'http': f'http://{login}:{password}@',
+            'https': f'http://{login}:{password}@'            
         },
         {
-            'http': f'socks5://{login}:{password}@',
-            'https': f'socks5://{login}:{password}@'            
+            'http': f'http://{login}:{password}@',
+            'https': f'http://{login}:{password}@'            
         },
         {
-            'http': f'socks5://{login}:{password}@',
-            'https': f'socks5://{login}:{password}@'            
+            'http': f'http://{login}:{password}@',
+            'https': f'http://{login}:{password}@'            
         },
-        {
-            'http': f'socks5://{login}:{password}@',
-            'https': f'socks5://{login}:{password}@'            
-        },
-        {
-            'http': f'socks5://{login}:{password}@',
-            'https': f'socks5://{login}:{password}@'            
-        },
-
-        ]
+    ]
     
     proxies = proxies_list[n]
     return proxies
 
+# получаем цену для продажи предмета с учётом комиссии
 def comparing(item_url,item,item_list):
-    
-    r = requests.get(item_url, headers=headers)
-    
-    if r.status_code != 200:
-        print('\n' + 'Подождите немного' + '\n')
-        time.sleep(45)
-        price_ob,price_ins = comparing(item_url,item,item_list)
-        
-        return price_ob, price_ins
-        
-    src = r.text
-    
-    soup = BeautifulSoup(src, "lxml")
-    
-    name_item = soup.find('a', class_='market_listing_row_link')
-    
-    price = name_item.find('span', class_='market_table_value normal_price')
-    price = price.find('span', class_='normal_price').text
-    price = re.findall(r'\d+', price) 
-    if len(price) == 1:
-        price = price + [0]
-    price = float(price[0]) + float(price[1])/100
-    
-    item_list[item]['price_ob'] = price
-    with open('items_list.json', 'w', encoding="utf-8") as file:
-        json.dump(item_list, file)
-    
-    return price
-
-def purchase_order_one(item_list,item,proxies,n):
-    
     try:
-        price_order_buy,n = purchase_order(item_list,item,proxies,n)
-    except:
-        n += 1
-        if n == 5:
-            n = 0
-        proxies = proxies_list(n)
-        print("Смена IP")
-        time.sleep(2)
-        price_order_buy,n = purchase_order_one(item_list,item,proxies,n)
+        r = requests.get(item_url, headers=headers)
+        src = r.text
+        soup = BeautifulSoup(src, "lxml")
+        
+        name_item = soup.find('a', class_='market_listing_row_link')
+        
+        price = name_item.find('span', class_='market_table_value normal_price')
+        price = price.find('span', class_='normal_price').text
+        price = re.findall(r'\d+', price) 
+        if len(price) == 1:
+            price = price + [0]
+        price = float(price[0]) + float(price[1])/100
+        
+        item_list[item]['price_ob'] = price
+        with open('items_list.json', 'w', encoding="utf-8") as file:
+            json.dump(item_list, file)
+        
+        return price
     
-    return price_order_buy,n
+    # переподключение при плохом соединении в Steam
+    except AttributeError:
+        print('Ошибка подключения ')
+        time.sleep(5)
+        price = comparing(item_url,item,item_list)
+        return price
 
+# парсим предметы на которые есть запрос на покупку
+def get_order_list():
+    url = 'https://steamcommunity.com/market/'
+    
+    data = []
+    
+    response = requests.get(url=url, headers=headers).text
+
+    soup = BeautifulSoup(response, "lxml")
+
+    src = soup.find_all('div', class_ = 'market_listing_row market_recent_listing_row')
+
+    for el in src:
+        item = el.find('a', class_='market_listing_item_name_link')
+        data.append(item.text)
+        
+    return data
+   
+# получаем цену для выставления запроса на покупку     
 def purchase_order(item_list,item,proxies,n):
     
     id = item_list[item]['id']
@@ -93,9 +89,7 @@ def purchase_order(item_list,item,proxies,n):
         'two_factor': '0',
     }
 
-    response = requests.get('https://steamcommunity.com/market/itemordershistogram', params=params, headers=headers, proxies=proxies).text
-
-    time.sleep(1)
+    response = requests.get('https://steamcommunity.com/market/itemordershistogram', params=params, headers=headers, proxies=proxies).text #, proxies=proxies,  timeout=5,
 
     soup = BeautifulSoup(response, "lxml")
     
@@ -108,8 +102,16 @@ def purchase_order(item_list,item,proxies,n):
     file = open("items_list.json", 'w')
     json.dump(item_list,file)
     file.close()
+    
+    # переводим из чтроки в число
+    price_order_buy = re.findall(r'\d+', price) 
+    if len(price_order_buy) == 1:
+        price_order_buy = price_order_buy + [0]
+    price_order_buy = float(price_order_buy[0]) + float(price_order_buy[1])/100
+    price_order_buy = price_order_buy*100 
+    price = price_order_buy + 2
+    
     return price,n
-
 
 def main():
     
@@ -121,36 +123,41 @@ def main():
         
     n = 0
     proxies = proxies_list(n)
-        
+    
+    listings_item = get_order_list()
+
     for item in item_list:
         urrr = item.replace('Inscribed ','')
         item_url = f'https://steamcommunity.com/market/search?category_570_Hero%5B%5D=any&category_570_Slot%5B%5D=any&category_570_Type%5B%5D=any&category_570_Quality[]=tag_unique&appid=570&q={urrr}'
         
-        listings_item = steam_client.market.get_my_market_listings()
         
         i = 0
-        for el in listings_item["buy_orders"]:
-            item_name = listings_item["buy_orders"][el]['item_name']
-            if item == item_name:
-                print(f'предмет {item} есть')
+        # проверяем есть ли педмет в списке покупок
+        for el in listings_item:
+
+            if item == el:
+                print(f'Предмет {item} ЕСТЬ')
                 i = 1
                 break
             
-        time.sleep(0.5)
+        time.sleep(1)
         
         if i == 0:
             
             price_ob = comparing(item_url,item,item_list)
             price_ob = price_ob * 100
-            price_order_buy,n = purchase_order_one(item_list,item,proxies,n)
+            try:
+                price_order_buy,n = purchase_order(item_list,item,proxies,n)
             
-            price_order_buy = re.findall(r'\d+', price_order_buy) 
-            if len(price_order_buy) == 1:
-                price_order_buy = price_order_buy + [0]
-            price_order_buy = float(price_order_buy[0]) + float(price_order_buy[1])/100
-            price_order_buy = price_order_buy*100 
-            price_order_buy = price_order_buy + 2
+            # смена IP адреса 
+            except IndexError:
+                n += 1
+                proxies = proxies_list(n)
+                print("Смена IP")
+                time.sleep(2)
+                price_order_buy,n = purchase_order(item_list,item,proxies,n)
             
+            # сравнение двух цен и выставление запроса
             if price_order_buy < (price_ob - (price_ob * 0.15)):
                 
                 response = steam_client.market.create_buy_order(f'{item}', f'{price_order_buy}', 1, GameOptions.DOTA2, Currency.RUB)
@@ -159,16 +166,11 @@ def main():
                 file = open("items_list.json", 'w')
                 json.dump(item_list,file)
                 file.close()
-
-                print(f"предмет выставлен {item}")
+                print(f"Предмет ВЫСТАВЛЕН {item}")
                 
-                asyncio.sleep(3000)
-                time.sleep(2)
             else:
-                print(f'цена не подходит ({item})')
-                asyncio.sleep(3000)
-                time.sleep(2)
-    
+                print(f'ЦЕНА НЕ ПОДХОДИТ ({item})')
+            
 if __name__ == "__main__":
     main()
-    
+        
