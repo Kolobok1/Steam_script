12484060a512b0fd7ba3bc0fa86bdd5a7adb1119
Kolobok1 <73997845+Kolobok1@users.noreply.github.com>
2023-08-07 13:39:18 +0300

    Add files via upload

---
+++ script_spec.py
@@ -0,0 +1,71 @@
+import json,pickle
+from steampy.models import GameOptions, Currency
+from bs4 import BeautifulSoup
+import requests, re
+import time
+from headers import headers
+
+def buy_item(steam_client,item,item_id,price):
+    print('нада')
+    response = steam_client.market.buy_item(item, item_id, 5, 15, GameOptions.DOTA2, Currency.RUB)
+
+def companing(item_url):
+    try:
+        price_data = []
+        
+        r = requests.get(item_url, headers=headers).text
+        soup = BeautifulSoup(r, "lxml")
+        price_items = soup.find_all('span', class_='market_table_value normal_price')
+        for price_item in price_items:
+            
+            price_item = price_item.find('span', class_='normal_price').text
+            price_item = re.findall(r'\d+', price_item) 
+            if len(price_item) == 1:
+                price_item = price_item + [0]
+            price_item = float(price_item[0]) + float(price_item[1])/100
+            price_data.append(price_item)
+        
+        # сделать распределение по цене
+        price = price_data[0]
+        price -= price * 0.13
+        price = round(price,2)
+        price_data[0] = price
+        if price_data[0] > price_data[1]:
+            # buy_item()
+            return price_data
+        else:
+            return None
+    except:
+        print('ошибка подключения')
+        time.sleep(5)
+        price_data = companing(item_url)
+    
+
+def main():
+    with open(f"steamClient.pkl", 'rb') as file:
+        steam_client = pickle.load(file)
+    
+    with open('items_list.json', 'r', encoding='utf-8') as fh:
+        item_list = json.load(fh)
+    
+    for item in item_list:
+        urrr = item.replace('Inscribed ','')
+        item_url = f'https://steamcommunity.com/market/search?category_570_Hero%5B%5D=any&category_570_Slot%5B%5D=any&category_570_Type%5B%5D=any&category_570_Quality[]=tag_unique&category_570_Quality[]=tag_strange&appid=570&q={urrr}'
+
+        price_data = companing(item_url)
+
+        if price_data != None:
+            # item_id = item_list[item]['id']
+            # price = price_data[0]
+            # buy_item(steam_client,item,item_id,price)
+            print(item,' ', item_list[item]['url'])
+            price = price_data[0] - price_data[1] 
+            price = round(price,2)
+            print(price, '\n')
+        else:
+            print('не нада')
+        # time.sleep(1)
+        
+if __name__ == "__main__":
+    main()
+    
